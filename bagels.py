837888457d5be228aaaeed99e64848dd98ffb130
Harnan <115539697+Harnan01@users.noreply.github.com>
2023-02-17 13:17:51 +0530

    Complete the code

--- bagels.py
+++ bagels.py
@@ -68,6 +68,39 @@ def getSecretNum():
     return secretNum
 
 
+def getClues(guess, secretNum):
+    """Returns a string with the pico, fermi, bagels clues for aguess and secret number pair."""
+    if guess == secretNum:
+        return 'you got it!'
+
+    clues =  []
+
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            #A correct digit is in the correct place.
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            # A correct digit is in the incorrect place.
+            clues.append('Pico')
+
+    if  len(clues) == 0:
+        return 'Bagels'  #There are no correct digit at all.
+    else:
+        # Sort the clues into alphabetical order so their original order
+        # doesn't give information away.
+
+        clues.sort()
+        # Make a single string from the list of string clues.
+        return ' '.join(clues)
+
+
+# If the program is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
+
+    
+
+
 
 
         
